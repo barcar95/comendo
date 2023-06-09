88c28a79daff0e329e53d47a823359b69ac8cd8a
artorrias <sam.henson1119@outlook.com>
2023-06-08 20:22:12 -0600

    started creating recipe cards from search engines

--- develop/script.js
+++ develop/script.js
@@ -6,7 +6,7 @@ var apiKey = "56471103c10dd6e34e3dab273edd9a51";
 var ident = "732afe70";
 
 function getApi(searchTerm) {
-    var recipeURL = "https://api.edamam.com/api/recipes/v2?app_key="+apiKey+"&app_id="+ident+"&q=chicken&type=public";
+    var recipeURL = "https://api.edamam.com/api/recipes/v2?app_key="+apiKey+"&app_id="+ident+"&q="+searchTerm+"&type=public";
     fetch(recipeURL).then(function (response) {
         if (response.ok) {
           response.json().then(function (data) {
@@ -37,10 +37,38 @@ var displayRecipes = function(repos) {
     }
     for (var i = 0; i < 10; i++) {
         //construct recipe card
+        var count = 1;
+        var recipeName = repos[i].recipe.label;
+        var recipeTitle = document.createElement("h1");
+        recipeTitle.classList = "title"
+        recipeTitle.textContent = recipeName;
+        var recipeCard = document.createElement('div');
+        recipeCard.classList = "card";
+        var recipeImg = document.createElement("img");
+        var recipeImgURL = repos[i].recipe.image;
+        recipeImg.setAttribute("src", recipeImgURL);
+        var recipeCalory = document.createElement("P");
+        recipeCalory.textContent ="Calories: "+repos[i].recipe.calories;
+        var recipeURL = document.createElement("p");
+        recipeURL.textContent = "URL: "+repos[i].recipe.url;
+
+        repoContainer.appendChild(recipeCard);
+        recipeCard.appendChild(recipeTitle);
+        recipeCard.appendChild(recipeImg);
+        recipeCard.appendChild(recipeCalory);
+        for (var y = 0; y<repos[i].recipe.ingredientLines.length; y++) {
+            var ingrList = document.createElement("span");
+            ingrList.textContent = count + ".) " + repos[i].recipe.ingredientLines[y]+", ";
+            count++;
+            recipeCard.appendChild(ingrList);
+        }
+        recipeCard.appendChild(recipeURL);
+
+
         console.log(repos[i].recipe.label);//title of dish
-        console.log(repos[i].recipe.images.regular.url);//picture of dish
+        console.log(repos[i].recipe.image);//picture of dish
         console.log(repos[i].recipe.totalTime);//time in minutes to cook and prep
-        console.log(repos[i].recipe.ingrediantLines);//array of ingrediants
+        console.log(repos[i].recipe.ingredientLines);//array of ingrediants
         console.log(repos[i].recipe.calories); //calories in the dish
         console.log(repos[i].recipe.url); //link to website for recipe
     }
